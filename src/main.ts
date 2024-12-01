82f3fd5fdf03a32a970dd42c8e916d9801af4730
root <root@3394029-mm70185.twc1.net>
2024-12-02 01:20:28 +0300

    configure deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
 
   const isProd = process.env.NODE_ENV === 'production';
